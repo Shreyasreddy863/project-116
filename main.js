2dbb2cf02739098809cdadc4a024cd3c6cfdef12
Shreyasreddy863 <78153431+Shreyasreddy863@users.noreply.github.com>
2021-07-12 18:27:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function prelod (){
+}
+
+function setup(){
+    canavas= createCnavas(300,300);
+    canavas.center();
+}
+
+function draw (){
+}
+
+function take_snapshot(){
+    SVGAElement('myFilterImage.png')
+}
